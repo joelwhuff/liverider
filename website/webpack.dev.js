74e517657ee0ec917a244d17b300b8e1072ac440
Joel <joelhuff.dev@gmail.com>
2022-01-08 21:34:33 -0500

    set up proper dev server

---
+++ website/webpack.dev.js
@@ -0,0 +1,21 @@
+import { join } from 'path';
+import { cwd } from 'process';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: join(cwd(), 'dist'),
+    },
+    devServer: { static: './dist' },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.(woff|woff2)/, type: 'asset/resource', generator: { filename: '[name][ext]' } },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/template.html' })],
+};
